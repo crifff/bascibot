07aac2a62d430f0d8451f897c541b15453bc46c5
hoshina85 <hoshina85@gmail.com>
2013-08-04 19:13:13 +0900

    update view

--- protected/views/checked/admin.php
+++ protected/views/checked/admin.php
@@ -12,43 +12,49 @@ $this->menu = array(
 	array('label' => 'Create Program', 'url' => array('create')),
 );
 
-Yii::app()->clientScript->registerScript('search', "
-$('.search-button').click(function(){
-	$('.search-form').toggle();
-	return false;
-});
-$('.search-form form').submit(function(){
-	$('#program-grid').yiiGridView('update', {
-		data: $(this).serialize()
+Yii::app()->clientScript->registerScript(
+	'search',
+	<<<'js'
+	$('.search-button').click(function(){
+		$('.search-form').toggle();
+		return false;
 	});
-	return false;
-});
-");
+	$('.search-form form').submit(function(){
+		$('#program-grid').yiiGridView('update', {
+			data: $(this).serialize()
+		});
+		return false;
+	});
+js
+);
 ?>
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id' => 'program-grid',
-	'dataProvider' => $model->checkedSearch(),
-	//	'filter' => $model,
-	'columns' => array(
-		//		'id',
-		array('name' => 'title', 'value' => '$data->title->title'),
-		array('name' => 'channel', 'value' => '$data->channel->name'),
-		/*
-		'subTitle',
-		'flag',
-		'deleted',
-		'warn',
-		'revision',
-		'allDay',
-		*/
-		array(
-			'class' => 'CButtonColumn',
-			'template' => '{delete}',
-			'deleteButtonImageUrl'=>'/images/cancel-circle.png',
+<?php $this->widget(
+	'zii.widgets.grid.CGridView',
+	array(
+		'id' => 'program-grid',
+		'dataProvider' => $model->checkedSearch(),
+		//	'filter' => $model,
+		'columns' => array(
+			//		'id',
+			array('name' => 'title', 'value' => '$data->title->title'),
+			array('name' => 'channel', 'value' => '$data->channel->name'),
+			/*
+			'subTitle',
+			'flag',
+			'deleted',
+			'warn',
+			'revision',
+			'allDay',
+			*/
+			array(
+				'class' => 'CButtonColumn',
+				'template' => '{delete}',
+				'deleteButtonImageUrl' => Yii::app()->request->baseUrl.'/images/cancel-circle.png',
+			),
 		),
-	),
-	'pager' => 'application.widgets.LinkPager',
-	'pagerCssClass' => 'pure-paginator',
-	'summaryText' => '',
-	'htmlOptions' => array('class' => 'pure-table pure-table-horizontal'),
-)); ?>
+		'pager' => 'application.widgets.LinkPager',
+		'pagerCssClass' => 'pure-paginator',
+		'summaryText' => '',
+		'htmlOptions' => array('class' => 'pure-table pure-table-horizontal'),
+	)
+); ?>
--- protected/views/program/admin.php
+++ protected/views/program/admin.php
@@ -1,8 +1,9 @@
 <?php
 /* @var $this ProgramController */
 /* @var $model Program */
-
-Yii::app()->clientScript->registerScript('search', "
+Yii::app()->clientScript->registerScript(
+	'search',
+	<<<'js'
 $('.search-button').click(function(){
 	$('.search-form').toggle();
 	return false;
@@ -13,64 +14,86 @@ $('.search-form form').keyup(function(){
 	});
 	return false;
 });
-");
+js
+);
 ?>
 
-
-
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id' => 'program-grid',
-	'dataProvider' => $model->search(), 'filter' => $model, 'columns' => array(
-		//		array('name' => 'category', 'value' => '$data->title->category'),
-		//		array('name' => '', 'value' => '$data->check?"✔":""', 'filter' => false, 'htmlOptions' => array('class' => 'item-checked')),
-		array('name' => 'title', 'value' => '$data->title->title', 'htmlOptions' => array('class' => 'item-title')),
-		array('name' => 'channel', 'value' => '$data->channel->name', 'htmlOptions' => array('class' => 'item-channel')),
-		//		'startTime',
-		array
-		(
-			'class' => 'CButtonColumn',
-			'template' => '{check}{uncheck}',
-			'buttons' => array
+<?php $this->widget(
+	'zii.widgets.grid.CGridView',
+	array(
+		'id' => 'program-grid',
+		'dataProvider' => $model->search(),
+		'filter' => $model,
+		'columns' => array(
+			//		array('name' => 'category', 'value' => '$data->title->category'),
+			//		array('name' => '', 'value' => '$data->check?"✔":""', 'filter' => false, 'htmlOptions' => array('class' => 'item-checked')),
+			array('name' => 'title', 'value' => '$data->title->title', 'htmlOptions' => array('class' => 'item-title')),
+			array(
+				'name' => 'channel',
+				'value' => '$data->channel->name',
+				'htmlOptions' => array('class' => 'item-channel')
+			),
+			//		'startTime',
+			array
 			(
-				'check' => array
+				'class' => 'CButtonColumn',
+				'template' => '{check}{uncheck}',
+				'buttons' => array
 				(
-					'label' => '<span class="icon-checkbox-unchecked"></span>',
-					'url' => 'Yii::app()->controller->createUrl("check",array("titleId"=>$data->title->id,"channelId"=>$data->channel->id))',
-					'click' => "function(){
+					'check' => array
+					(
+						'label' => '<span class="icon-checkbox-unchecked"></span>',
+						'url' => function ($data) {
+							return Yii::app()->controller->createUrl(
+								"check",
+								array("titleId" => $data->title->id, "channelId" => $data->channel->id)
+							);
+						},
+						'click' => <<<'js'
+function postCheckProgram(){
     $.fn.yiiGridView.update('program-grid', {
         type:'POST',
         url:$(this).attr('href'),
-        success:function(data) {
+        success:function() {
               $.fn.yiiGridView.update('program-grid');
         }
-    })
+    });
     return false;
   }
-",
-					'visible' => '!$data->check'
-				),
-				'uncheck' => array
-				(
-					'label' => '<span class="icon-checkbox-checked"></span>',
-					'url' => 'Yii::app()->controller->createUrl("uncheck",array("titleId"=>$data->title->id,"channelId"=>$data->channel->id))',
-					'click' => "function(){
+js
+					,
+						'visible' => '!$data->check'
+					),
+					'uncheck' => array
+					(
+						'label' => '<span class="icon-checkbox-checked"></span>',
+						'url' => function ($data) {
+							return Yii::app()->controller->createUrl(
+								"uncheck",
+								array("titleId" => $data->title->id, "channelId" => $data->channel->id)
+							);
+						},
+						'click' => <<<'js'
+function postUnCheckProgram(){
     $.fn.yiiGridView.update('program-grid', {
         type:'POST',
         url:$(this).attr('href'),
-        success:function(data) {
+        success:function() {
               $.fn.yiiGridView.update('program-grid');
         }
-    })
+    });
     return false;
   }
-",
-					'visible' => '$data->check'
+js
+					,
+						'visible' => '$data->check'
+					),
 				),
 			),
 		),
-	),
-	'pager' => 'application.widgets.LinkPager',
-	'pagerCssClass' => 'pure-paginator',
-	'summaryText' => '',
-	'htmlOptions' => array('class' => 'pure-table pure-table-horizontal'),
-));?>
+		'pager' => 'application.widgets.LinkPager',
+		'pagerCssClass' => 'pure-paginator',
+		'summaryText' => '',
+		'htmlOptions' => array('class' => 'pure-table pure-table-horizontal'),
+	)
+);?>
