736936bea69608d4634ad38a7a7bdd2402720a6f
hoshina85 <hoshina85@gmail.com>
2013-08-04 19:38:29 +0900

    hide index.php

--- protected/config/main.php
+++ protected/config/main.php
@@ -1,67 +1,64 @@
 <?php
 
 return CMap::mergeArray(
-  require(dirname(__FILE__) . '/database.php'),
-  array(
-    'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-    'name'=>'bascibot {\'<\'}',
-
-    'preload'=>array('log'),
-
-    'import'=>array(
-      'application.models.*',
-      'application.components.*',
-    ),
-
-    'modules'=>array(
-      'gii'=>array(
-        'class'=>'system.gii.GiiModule',
-        'password'=>'hoge',
-      ),
-    ),
-
-    'components'=>array(
-      'user'=>array(
-        'allowAutoLogin'=>true,
-      ),
-      'urlManager'=>array(
-        'urlFormat'=>'path',
-        'rules'=>array(
-          '<controller:\w+>/<id:\d+>'=>'<controller>/view',
-          '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-          '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-        ),
-      ),
-      'errorHandler'=>array(
-        'errorAction'=>'site/error',
-      ),
-      'log'=>array(
-        'class'=>'CLogRouter',
-        'routes'=>array(
-          array(
-            'class'=>'CFileLogRoute',
-            'levels'=>'error, warning',
-          ),
-        ),
-      ),
-      'widgetFactory'=>array(
-        'widgets'=>array(
-          'CLinkPager'=>array(
-            'maxButtonCount'=>5,
-            'cssFile'=>false,
-          ),
-          'CGridView'=>array(
-            'cssFile'=>false,
-          ),
-          'CJuiDatePicker'=>array(
-            'language'=>'ja',
-          ),
-        ),
-      ),
-    ),
-
-    'params'=>array(
-      'adminEmail'=>'webmaster@example.com',
-    ),
-  )
+	require(dirname(__FILE__) . '/database.php'),
+	array(
+		'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+		'name' => 'bascibot {\'<\'}',
+		'preload' => array('log'),
+		'import' => array(
+			'application.models.*',
+			'application.components.*',
+		),
+		'modules' => array(
+			'gii' => array(
+				'class' => 'system.gii.GiiModule',
+				'password' => 'hoge',
+			),
+		),
+		'components' => array(
+			'user' => array(
+				'allowAutoLogin' => true,
+			),
+			'urlManager' => array(
+				'urlFormat' => 'path',
+				'rules' => array(
+					'<controller:\w+>/' => '<controller>/index',
+					'<controller:\w+>/<id:\d+>' => '<controller>/view',
+					'<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+					'<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+				),
+				'showScriptName' => false,
+			),
+			'errorHandler' => array(
+				'errorAction' => 'site/error',
+			),
+			'log' => array(
+				'class' => 'CLogRouter',
+				'routes' => array(
+					array(
+						'class' => 'CFileLogRoute',
+						'levels' => 'error, warning',
+					),
+				),
+			),
+			'widgetFactory' => array(
+				'widgets' => array(
+					'CLinkPager' => array(
+						'maxButtonCount' => 5,
+						'cssFile' => false,
+					),
+					'CGridView' => array(
+						'cssFile' => false,
+					),
+					'CJuiDatePicker' => array(
+						'language' => 'ja',
+					),
+				),
+			),
+		),
+		'params' => array(
+			'adminEmail' => 'webmaster@example.com',
+		),
+	)
 );
--- protected/config/test.php
+++ protected/config/test.php
@@ -4,6 +4,9 @@ return CMap::mergeArray(
 	require(dirname(__FILE__) . '/main.php'),
 	array(
 		'components' => array(
+			'urlManager' => array(
+				'showScriptName' => true,
+			),
 			'fixture' => array(
 				'class' => 'system.test.CDbFixtureManager',
 			),
